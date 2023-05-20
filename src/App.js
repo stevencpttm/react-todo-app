d8a2bd9e12dd511a9c3c654da649893e0db12ba9
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2023-05-20 15:34:45 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      { id: 1, title: "Item AAAAAAAA", done: false },
+      { id: 2, title: "Item B", done: false },
+    ],
+    newTodoTitle: "",
+  };
+
+  addItemHandler = (e) => {
+    this.setState({
+      newTodoTitle: e.target.value,
+    });
+  };
+
+  addHandler = (e) => {
+    const newTodos = [...this.state.todos];
+    newTodos.push({
+      id: this.state.todos.length + 1,
+      title: this.state.newTodoTitle,
+      done: false,
+    });
+
+    this.setState({
+      todos: newTodos,
+      newTodoTitle: "",
+    });
+  };
+
+  toggleState = (todoItemId) => {
+    const newTodos = [...this.state.todos];
+    const theItem = newTodos.find((todo) => todo.id === todoItemId);
+
+    theItem.done = !theItem.done;
+
+    this.setState({
+      todos: newTodos,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>TODO App</h1>
+        <ul>
+          {this.state.todos.map((todo) => {
+            return (
+              <li
+                key={todo.id}
+                onClick={(e) => this.toggleState(todo.id)}
+                className={`${todo.done ? "done" : ""}`}
+              >
+                {todo.title}
+              </li>
+            );
+          })}
+        </ul>
+        <input
+          type="text"
+          value={this.state.newTodoTitle}
+          onChange={this.addItemHandler}
+          placeholder="Todo Item..."
+        />
+        <button onClick={this.addHandler}>Add</button>
+      </div>
+    );
+  }
 }
 
 export default App;
